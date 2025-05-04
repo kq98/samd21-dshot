19a75b44af5247f6e63eed1eaa98b9b3646ce827
Konstantin Quillfeldt <k-q@web.de>
2025-05-04 16:29:39 +0200

    Dynamic dimming of Pin 13 works

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 
 static int _writeResolution = 8;
 
+int counter = 0;
+
 // Wait for synchronization of registers between the clock domains
 static __inline__ void syncTC_16(Tc* TCx) __attribute__((always_inline, unused));
 static void syncTC_16(Tc* TCx) {
@@ -13,29 +15,6 @@ static void syncTC_16(Tc* TCx) {
 static __inline__ void syncTCC(Tcc* TCCx) __attribute__((always_inline, unused));
 static void syncTCC(Tcc* TCCx) {
   while (TCCx->SYNCBUSY.reg & TCC_SYNCBUSY_MASK);
-}
-
-int pinPeripheral( uint32_t ulPin, EPioType ulPeripheral )
-{
-  if ( g_APinDescription[ulPin].ulPin & 1 ) // is pin odd?
-  {
-    uint32_t temp ;
-
-    // Get whole current setup for both odd and even pins and remove odd one
-    temp = (PORT->Group[g_APinDescription[ulPin].ulPort].PMUX[g_APinDescription[ulPin].ulPin >> 1].reg) & PORT_PMUX_PMUXE( 0xF ) ;
-    // Set new muxing
-    PORT->Group[g_APinDescription[ulPin].ulPort].PMUX[g_APinDescription[ulPin].ulPin >> 1].reg = temp|PORT_PMUX_PMUXO( ulPeripheral ) ;
-    // Enable port mux
-    PORT->Group[g_APinDescription[ulPin].ulPort].PINCFG[g_APinDescription[ulPin].ulPin].reg |= PORT_PINCFG_PMUXEN ;
-  }
-  else // even pin
-  {
-    uint32_t temp ;
-
-    temp = (PORT->Group[g_APinDescription[ulPin].ulPort].PMUX[g_APinDescription[ulPin].ulPin >> 1].reg) & PORT_PMUX_PMUXO( 0xF ) ;
-    PORT->Group[g_APinDescription[ulPin].ulPort].PMUX[g_APinDescription[ulPin].ulPin >> 1].reg = temp|PORT_PMUX_PMUXE( ulPeripheral ) ;
-    PORT->Group[g_APinDescription[ulPin].ulPort].PINCFG[g_APinDescription[ulPin].ulPin].reg |= PORT_PINCFG_PMUXEN ; // Enable port mux
-  }
 }
 
 static inline uint32_t mapResolution(uint32_t value, uint32_t from, uint32_t to)
@@ -49,106 +28,69 @@ static inline uint32_t mapResolution(uint32_t value, uint32_t from, uint32_t to)
   return value << (to-from);
 }
 
-void setup() {
-  int pin = 13;
+void myAnalogWrite(int pin, int value) {
 
   PinDescription pinDesc = g_APinDescription[pin];
-  uint32_t attr = pinDesc.ulPinAttribute;
 
-  int value = 30;
+  value = mapResolution(value, _writeResolution, 16);
 
-  if ((attr & PIN_ATTR_PWM) == PIN_ATTR_PWM)
-  {
-    value = mapResolution(value, _writeResolution, 16);
+  uint32_t tcNum = GetTCNumber(pinDesc.ulPWMChannel);
+  static bool tcEnabled[TCC_INST_NUM+TC_INST_NUM];
 
-    uint32_t tcNum = GetTCNumber(pinDesc.ulPWMChannel);
-    uint8_t tcChannel = GetTCChannelNumber(pinDesc.ulPWMChannel);
-    static bool tcEnabled[TCC_INST_NUM+TC_INST_NUM];
+  // Get whole current setup for both odd and even pins and remove odd one
+  // uint32_t temp = (PORT->Group[g_APinDescription[pin].ulPort].PMUX[g_APinDescription[pin].ulPin >> 1].reg) & PORT_PMUX_PMUXE( 0xF ) ;
+  // Set new muxing
+  // PORT->Group[g_APinDescription[pin].ulPort].PMUX[g_APinDescription[pin].ulPin >> 1].reg = temp|PORT_PMUX_PMUXO( PIO_TIMER ) ;
 
-    if (attr & PIN_ATTR_TIMER) {
-      #if !(ARDUINO_SAMD_VARIANT_COMPLIANCE >= 10603)
-      // Compatibility for cores based on SAMD core <=1.6.2
-      if (pinDesc.ulPinType == PIO_TIMER_ALT) {
-        pinPeripheral(pin, PIO_TIMER_ALT);
-      } else
-      #endif
-      {
-        pinPeripheral(pin, PIO_TIMER);
-      }
-    } else {
-      // We suppose that attr has PIN_ATTR_TIMER_ALT bit set...
-      pinPeripheral(pin, PIO_TIMER_ALT);
-    }
+  PORT->Group[g_APinDescription[pin].ulPort].PMUX[g_APinDescription[pin].ulPin >> 1].reg = PORT_PMUX_PMUXO_F;
+  PORT->Group[g_APinDescription[pin].ulPort].PINCFG[g_APinDescription[pin].ulPin].reg |= PORT_PINCFG_PMUXEN ;
 
-    if (!tcEnabled[tcNum]) {
-      tcEnabled[tcNum] = true;
+  if (!tcEnabled[tcNum]) {
+    tcEnabled[tcNum] = true;
 
-      uint16_t GCLK_CLKCTRL_IDs[] = {
-        GCLK_CLKCTRL_ID(GCM_TCC0_TCC1), // TCC0
-        GCLK_CLKCTRL_ID(GCM_TCC0_TCC1), // TCC1
-        GCLK_CLKCTRL_ID(GCM_TCC2_TC3),  // TCC2
-        GCLK_CLKCTRL_ID(GCM_TCC2_TC3),  // TC3
-        GCLK_CLKCTRL_ID(GCM_TC4_TC5),   // TC4
-        GCLK_CLKCTRL_ID(GCM_TC4_TC5),   // TC5
-        GCLK_CLKCTRL_ID(GCM_TC6_TC7),   // TC6
-        GCLK_CLKCTRL_ID(GCM_TC6_TC7),   // TC7
-      };
-      GCLK->CLKCTRL.reg = (uint16_t) (GCLK_CLKCTRL_CLKEN | GCLK_CLKCTRL_GEN_GCLK0 | GCLK_CLKCTRL_IDs[tcNum]);
-      while (GCLK->STATUS.bit.SYNCBUSY == 1);
+    GCLK->CLKCTRL.reg = (uint16_t) (GCLK_CLKCTRL_CLKEN | GCLK_CLKCTRL_GEN_GCLK0 | GCLK_CLKCTRL_ID(GCM_TCC0_TCC1));
+    while (GCLK->STATUS.bit.SYNCBUSY == 1);
 
-      // Set PORT
-      if (tcNum >= TCC_INST_NUM) {
-        // -- Configure TC
-        Tc* TCx = (Tc*) GetTC(pinDesc.ulPWMChannel);
-        // Disable TCx
-        TCx->COUNT16.CTRLA.bit.ENABLE = 0;
-        syncTC_16(TCx);
-        // Set Timer counter Mode to 16 bits, normal PWM
-        TCx->COUNT16.CTRLA.reg |= TC_CTRLA_MODE_COUNT16 | TC_CTRLA_WAVEGEN_NPWM;
-        syncTC_16(TCx);
-        // Set the initial value
-        TCx->COUNT16.CC[tcChannel].reg = (uint32_t) value;
-        syncTC_16(TCx);
-        // Enable TCx
-        TCx->COUNT16.CTRLA.bit.ENABLE = 1;
-        syncTC_16(TCx);
-      } else {
-        // -- Configure TCC
-        Tcc* TCCx = (Tcc*) GetTC(pinDesc.ulPWMChannel);
-        // Disable TCCx
-        TCCx->CTRLA.bit.ENABLE = 0;
-        syncTCC(TCCx);
-        // Set TCCx as normal PWM
-        TCCx->WAVE.reg |= TCC_WAVE_WAVEGEN_NPWM;
-        syncTCC(TCCx);
-        // Set the initial value
-        TCCx->CC[tcChannel].reg = (uint32_t) value;
-        syncTCC(TCCx);
-        // Set PER to maximum counter value (resolution : 0xFFFF)
-        TCCx->PER.reg = 0xFFFF;
-        syncTCC(TCCx);
-        // Enable TCCx
-        TCCx->CTRLA.bit.ENABLE = 1;
-        syncTCC(TCCx);
-      }
-    } else {
-      if (tcNum >= TCC_INST_NUM) {
-        Tc* TCx = (Tc*) GetTC(pinDesc.ulPWMChannel);
-        TCx->COUNT16.CC[tcChannel].reg = (uint32_t) value;
-        syncTC_16(TCx);
-      } else {
-        Tcc* TCCx = (Tcc*) GetTC(pinDesc.ulPWMChannel);
-        TCCx->CTRLBSET.bit.LUPD = 1;
-        syncTCC(TCCx);
-        TCCx->CCB[tcChannel].reg = (uint32_t) value;
-        syncTCC(TCCx);
-        TCCx->CTRLBCLR.bit.LUPD = 1;
-        syncTCC(TCCx);
-      }
-    }
+    // Set PORT
+
+    // -- Configure TCC
+    Tcc* TCCx = TCC0; //(Tcc*) GetTC(pinDesc.ulPWMChannel);
+    // Disable TCCx
+    TCCx->CTRLA.bit.ENABLE = 0;
+    syncTCC(TCCx);
+    // Set TCCx as normal PWM
+    TCCx->WAVE.reg |= TCC_WAVE_WAVEGEN_NPWM;
+    syncTCC(TCCx);
+    // Set the initial value
+    TCCx->CC[3].reg = (uint32_t) value;
+    syncTCC(TCCx);
+    // Set PER to maximum counter value (resolution : 0xFFFF)
+    TCCx->PER.reg = 0xFFFF;
+    syncTCC(TCCx);
+    // Enable TCCx
+    TCCx->CTRLA.bit.ENABLE = 1;
+    syncTCC(TCCx);
+  } else {
+      Tcc* TCCx = TCC0; //(Tcc*) GetTC(pinDesc.ulPWMChannel);
+      TCCx->CTRLBSET.bit.LUPD = 1;
+      syncTCC(TCCx);
+      TCCx->CCB[3].reg = (uint32_t) value;
+      syncTCC(TCCx);
+      TCCx->CTRLBCLR.bit.LUPD = 1;
+      syncTCC(TCCx);
   }
+}
+
+void setup() {
+  myAnalogWrite(13, 128);
 }
 
 void loop() {
 
+  myAnalogWrite(13, counter);
+  delay ( 10 );
+  if (counter < 255)
+    counter++;
+  else 
+    counter = 0;
 }
