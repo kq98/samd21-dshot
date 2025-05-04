9d05cf9127b7081dde62425f04308efea6fd676a
Konstantin Quillfeldt <k-q@web.de>
2025-05-04 16:29:39 +0200

    DMA working on 4 pins with linked Descriptors

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,8 @@ static int _writeResolution = 8;
 
 int counter[4] = {0, 0, 0, 0};
 
-int led = 1;
+uint8_t DSHOT600_HIGH = 60;
+uint8_t DSHOT600_LOW = 30;
 
 struct dmaDescriptor {
   uint16_t btctrl;
@@ -16,10 +17,17 @@ struct dmaDescriptor {
   uint32_t descaddr;
 };
 
-uint8_t motorDSHOT_Frame[16] __attribute__ ((aligned (16))) = {0,10,20,30,40,50,60,70,100,130,140,150,160,180,200,255}; 
+struct DSHOTFrames {
+  uint8_t motor1[16] __attribute__ ((aligned (16))) = {80,75,70,65,60,55,50,45,40,35,30,25,20,15,10,0};
+  uint8_t motor2[16] __attribute__ ((aligned (16))) = {80,75,70,65,60,55,50,45,40,35,30,25,20,15,10,0};
+  uint8_t motor3[16] __attribute__ ((aligned (16))) = {0,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80};
+  uint8_t motor4[16] __attribute__ ((aligned (16))) = {0,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80};
+};
 
-volatile dmaDescriptor dmaDescriptorArray[1] __attribute__ ((aligned (16)));
-dmaDescriptor dmaDescriptorWritebackArray[1] __attribute__ ((aligned (16)));
+DSHOTFrames dshotFrames;
+
+volatile dmaDescriptor dmaDescriptorArray[4] __attribute__ ((aligned (16)));
+dmaDescriptor dmaDescriptorWritebackArray[4] __attribute__ ((aligned (16)));
 
 // Wait for synchronization of registers between the clock domains
 static __inline__ void syncTC_16(Tc* TCx) __attribute__((always_inline, unused));
@@ -50,7 +58,7 @@ void myAnalogWrite(int pin, int value, uint8_t group, uint8_t channel) {
   uint32_t attr = pinDesc.ulPinAttribute;
 
 
-  value = mapResolution(value, _writeResolution, 8);
+  value = mapResolution(value, _writeResolution, 6);
 
   uint32_t tcNum = GetTCNumber(pinDesc.ulPWMChannel);
   static bool tcEnabled[TCC_INST_NUM+TC_INST_NUM];
@@ -69,8 +77,6 @@ void myAnalogWrite(int pin, int value, uint8_t group, uint8_t channel) {
     // We suppose that attr has PIN_ATTR_TIMER_ALT bit set...
     pinPeripheral(pin, PIO_TIMER_ALT);
   }
-
-  //PORT->Group[g_APinDescription[pin].ulPort].PINCFG[g_APinDescription[pin].ulPin].reg |= PORT_PINCFG_PMUXEN ;
 
   if (!tcEnabled[tcNum]) {
     tcEnabled[tcNum] = true;
@@ -91,19 +97,19 @@ void myAnalogWrite(int pin, int value, uint8_t group, uint8_t channel) {
     TCCx->CC[channel].reg = (uint32_t) value;
     syncTCC(TCCx);
     // Set PER to maximum counter value (resolution : 0xFFFF)
-    TCCx->PER.reg = 0x00FF;
+    TCCx->PER.reg = 0x0050; //48 MHz * 1,67 us = 80,16 appr. = 80 = 0x50
     syncTCC(TCCx);
     // Enable TCCx
     TCCx->CTRLA.bit.ENABLE = 1;
     syncTCC(TCCx);
   } else {
       Tcc* TCCx = TCC0; //(Tcc*) GetTC(pinDesc.ulPWMChannel);
-      // TCCx->CTRLBSET.bit.LUPD = 1;
-      // syncTCC(TCCx);
+      TCCx->CTRLBSET.bit.LUPD = 1;
+      syncTCC(TCCx);
       TCCx->CCB[channel].reg = (uint32_t) value;
       syncTCC(TCCx);
-      // TCCx->CTRLBCLR.bit.LUPD = 1;
-      // syncTCC(TCCx);
+      TCCx->CTRLBCLR.bit.LUPD = 1;
+      syncTCC(TCCx);
   }
 }
 
@@ -111,14 +117,7 @@ void DMAC_Handler() {
   // Must clear this flag! Otherwise the interrupt will be triggered over and over again.
   DMAC->CHINTFLAG.reg = DMAC_CHINTENCLR_MASK;
 
-  digitalWrite(LED_BUILTIN, led);
-
   DMAC->CHCTRLA.reg |= DMAC_CHCTRLA_ENABLE;
-
-  if (led == 0)
-    led = 1;
-  else
-    led = 0;
 }
 
 void setupDMA() {
@@ -130,7 +129,41 @@ void setupDMA() {
                                   (0 << 13);  // STEPSIZE=X1: Address Increment Step Size
   dmaDescriptorArray[0].btcnt = 16; // beat count
   dmaDescriptorArray[0].dstaddr = (uint32_t) &(TCC0->CCB[0].reg);
-  dmaDescriptorArray[0].srcaddr = (uint32_t) &motorDSHOT_Frame[16];
+  dmaDescriptorArray[0].srcaddr = (uint32_t) &dshotFrames.motor1[16];
+  dmaDescriptorArray[0].descaddr = (uint32_t) &dmaDescriptorArray[1];
+
+  dmaDescriptorArray[1].btctrl =  (1 << 0) |  // VALID: Descriptor Valid
+                                  (0 << 3) |  // BLOCKACT=NOACT: Block Action
+                                  (1 << 10) | // SRCINC: Source Address Increment Enable
+                                  (0 << 11) | // DSTINC: Destination Address Increment Enable
+                                  (1 << 12) | // STEPSEL=SRC: Step Selection
+                                  (0 << 13);  // STEPSIZE=X1: Address Increment Step Size
+  dmaDescriptorArray[1].btcnt = 16; // beat count
+  dmaDescriptorArray[1].dstaddr = (uint32_t) &(TCC0->CCB[1].reg);
+  dmaDescriptorArray[1].srcaddr = (uint32_t) &dshotFrames.motor2[16];
+  dmaDescriptorArray[1].descaddr = (uint32_t) &dmaDescriptorArray[2];
+
+  dmaDescriptorArray[2].btctrl =  (1 << 0) |  // VALID: Descriptor Valid
+                                  (0 << 3) |  // BLOCKACT=NOACT: Block Action
+                                  (1 << 10) | // SRCINC: Source Address Increment Enable
+                                  (0 << 11) | // DSTINC: Destination Address Increment Enable
+                                  (1 << 12) | // STEPSEL=SRC: Step Selection
+                                  (0 << 13);  // STEPSIZE=X1: Address Increment Step Size
+  dmaDescriptorArray[2].btcnt = 16; // beat count
+  dmaDescriptorArray[2].dstaddr = (uint32_t) &(TCC0->CCB[2].reg);
+  dmaDescriptorArray[2].srcaddr = (uint32_t) &dshotFrames.motor3[16];
+  dmaDescriptorArray[2].descaddr = (uint32_t) &dmaDescriptorArray[3];
+
+  dmaDescriptorArray[3].btctrl =  (1 << 0) |  // VALID: Descriptor Valid
+                                  (0 << 3) |  // BLOCKACT=NOACT: Block Action
+                                  (1 << 10) | // SRCINC: Source Address Increment Enable
+                                  (0 << 11) | // DSTINC: Destination Address Increment Enable
+                                  (1 << 12) | // STEPSEL=SRC: Step Selection
+                                  (0 << 13);  // STEPSIZE=X1: Address Increment Step Size
+  dmaDescriptorArray[3].btcnt = 16; // beat count
+  dmaDescriptorArray[3].dstaddr = (uint32_t) &(TCC0->CCB[3].reg);
+  dmaDescriptorArray[3].srcaddr = (uint32_t) &dshotFrames.motor4[16];
+  dmaDescriptorArray[3].descaddr = (uint32_t) 0x00; // last of the 4 linked descriptors
 
   DMAC->BASEADDR.reg = (uint32_t)dmaDescriptorArray;
   DMAC->WRBADDR.reg = (uint32_t)dmaDescriptorWritebackArray;
@@ -152,40 +185,47 @@ void setupDMA() {
 
 void setup() {
 
-  myAnalogWrite(4, 255, PORT_PMUX_PMUXE_E, 0); //PA08
+  myAnalogWrite(4, 0, PORT_PMUX_PMUXE_E, 0); //PA08
+  myAnalogWrite(3, 0, PORT_PMUX_PMUXO_F, 1); //PA09
+  myAnalogWrite(6, 0, PORT_PMUX_PMUXE_F, 2); //PA20
+  myAnalogWrite(7, 0, PORT_PMUX_PMUXO_F, 3); //PA21
+
   setupDMA();
-  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
 
   //myAnalogWrite(4, counter[0], PORT_PMUX_PMUXE_E, 0); //PA08
-  myAnalogWrite(3, counter[1], PORT_PMUX_PMUXO_F, 1); //PA09
-  myAnalogWrite(6, counter[2], PORT_PMUX_PMUXE_F, 2); //PA20
-  myAnalogWrite(7, counter[3], PORT_PMUX_PMUXO_F, 3); //PA21
+  // myAnalogWrite(3, counter[1], PORT_PMUX_PMUXO_F, 1); //PA09
+  // myAnalogWrite(6, counter[2], PORT_PMUX_PMUXE_F, 2); //PA20
+  // myAnalogWrite(7, counter[3], PORT_PMUX_PMUXO_F, 3); //PA21
   // analogWrite(5,counter);
   delay ( 10 );
-  if (counter[0] < 255) {
+  if (counter[0] < 80) {
     counter[0]++;
-    for(int i = 0; i < 16; i++) {
-      motorDSHOT_Frame[i] = counter[0];
-    }
   }
   else 
     counter[0] = 0;
 
-  if (counter[1] < 255)
+  if (counter[1] < 80)
     counter[1] += 10;
   else 
     counter[1] = 0;
 
-  if (counter[2] < 255)
+  if (counter[2] < 80)
     counter[2] += 20;
   else 
     counter[2] = 0;
 
-  if (counter[3] < 255)
+  if (counter[3] < 80)
     counter[3] += 50;
   else 
     counter[3] = 0;
+
+  for(int i = 0; i < 16; i++) {
+    dshotFrames.motor1[i] = counter[0];
+    dshotFrames.motor2[i] = counter[1];
+    dshotFrames.motor3[i] = counter[2];
+    dshotFrames.motor4[i] = counter[3];
+  }
 }
